935d3407686c3878f6b656448eb2267d3bc200b8
MissterMan <putrayogi4545@gmail.com>
2022-01-26 00:54:55 +0700

    change background color and text color

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,7 @@ module.exports = {
       backgroundImage: {
         'graph': "url('../img/graph-paper.svg')",
         'topo': "url('../img/topography.svg')",
+        'topo-dark': "url('../img/topography copy.svg')",
         'blob': "url('../img/blob.svg')",
       },
       colors: {
@@ -14,6 +15,10 @@ module.exports = {
         'fourth': '#515E63',
         'custom-yellow': '#F8CF3E',
         'custom-yellow-bright': '#FBE186',
+        'neon-dark': '#170055',
+        'neon-primary': '#3E00FF',
+        'neon-second': '#AE00FB',
+        'neon-third': '#B5FFD9',
       },
       fontFamily: {
         clash: ['ClashDisplay-Variable', 'sans-serif'],

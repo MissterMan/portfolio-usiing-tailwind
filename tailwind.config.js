42e03353a1b670eb7f0f0e894ba70d6219ac39dc
Pratama Putra Prayogi <putrayogi4545@gmail.com>
2022-04-01 23:43:26 +0700

    Major Update
    - Change hero view
    - Adding navbar section
    - Adding social media links
    - Adding about section
    - Not responsive yet!!!

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,29 +1,29 @@
 module.exports = {
-  content: ['./public/**/*.{html,js}'],
+  content: ["./public/**/*.{html,js}"],
   theme: {
     extend: {
       backgroundImage: {
-        'graph': "url('../img/graph-paper.svg')",
-        'topo': "url('../img/topography.svg')",
-        'topo-dark': "url('../img/topography copy.svg')",
-        'blob': "url('../img/blob.svg')",
+        graph: "url('../img/graph-paper.svg')",
+        topo: "url('../img/topography.svg')",
+        "topo-dark": "url('../img/topography copy.svg')",
+        blob: "url('../img/blob.svg')",
       },
       colors: {
-        'first': '#F1ECC3',
-        'second': '#C9D8B6',
-        'third': '#57837B',
-        'fourth': '#515E63',
-        'custom-yellow': '#F8CF3E',
-        'custom-yellow-bright': '#FBE186',
-        'neon-dark': '#170055',
-        'neon-primary': '#3E00FF',
-        'neon-second': '#AE00FB',
-        'neon-third': '#B5FFD9',
+        first: "#F1ECC3",
+        second: "#C9D8B6",
+        third: "#57837B",
+        fourth: "#515E63",
+        "custom-yellow": "#F8CF3E",
+        "custom-yellow-bright": "#FBE186",
+        "neon-dark": "#170055",
+        "neon-primary": "#3E00FF",
+        "neon-second": "#AE00FB",
+        "neon-third": "#B5FFD9",
       },
       fontFamily: {
-        clash: ['ClashDisplay-Variable', 'sans-serif'],
+        clash: ["ClashDisplay-Variable", "sans-serif"],
       },
     },
   },
   plugins: [],
-}
+};
